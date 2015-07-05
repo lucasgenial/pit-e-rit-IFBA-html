114cd95b77ac06221f7df1b16b3aa46e6aefa49f
wallace <wallace@Lenovo-G485>
2015-07-05 20:06:35 -0300

    Fizemos alterações no js de validações. Demos um jeito de deixar apenas uma função para todas as validações de datas, modificamos a função de validação de telefone e celular para não aceitar letras e outras coisas. Aí atualizamos os furmulários com as chamadas das funções.  OBS: Esse projeto tá atualizado. Baixamos o que você postou no GITHUB e modificamos.

--- public_html/Validacoes/validacoes.js
+++ public_html/Validacoes/validacoes.js
@@ -1,4 +1,5 @@
 function validaTelefoneCelular(telefone) {
+
     if (telefone.value.length == 0) {
         telefone.value = '(' + telefone.value;
     }
@@ -15,6 +16,7 @@ function validaTelefoneCelular(telefone) {
         //
         telefone.value = telefone.value.substr(0, 10) + "-" + telefone.value.substr(10, telefone.value.lengt);
     }
+
 
 }
 
@@ -66,17 +68,36 @@ $(function () {
 
 //------------------------------------------
 //Formatação e validação:
-function mascara_Validacao_Data(campoData) {
+function mascara_Validacao_Data(campoData, data1, data2) {
+    //MÁSCARA:
     if (campoData.value.length == 2) {
         campoData.value = campoData.value + '/'
     }
     if (campoData.value.length == 5) {
         campoData.value = campoData.value + '/';
     }
+    //----- COMPARAÇÃO DATAS:
+
+    if (campoData.value.length == 10) {
+        var data_1 = data1.value;
+        var data_2 = data2.value;
+
+        var Compara01 = parseInt(data_1.split("/")[2].toString() + data_1.split("/")[1].toString() + data_1.split("/")[0].toString());
+        var Compara02 = parseInt(data_2.split("/")[2].toString() + data_2.split("/")[1].toString() + data_2.split("/")[0].toString());
+        //
+        if ((Compara01 < Compara02) || (Compara01 == Compara02)) {
+
+        } else {
+
+            alert("Data final deve ser maior que a inicial!");
+            campoData.value = "";
+        }
+    }
 }
 //--------------------------------------------
 function Cancelar(pagina) {
     var confirmacao = confirm("Você deseja mesmo cancelar?");
+
 
     if (confirmacao) {
         window.location.href = pagina;
@@ -87,3 +108,14 @@ function Cancelar(pagina) {
     }
 
 }
+//-------------------------------------------
+//function validaSelects()
+//{
+//    if (document.form1.sel.value == "")
+//    {
+//        window.alert("Escolha uma opção!");
+//        document.form1.sel.focus();
+//    }
+//
+//}
+
